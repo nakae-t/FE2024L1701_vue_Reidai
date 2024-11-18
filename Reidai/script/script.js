c8028229ea9599d0c13688beee64da2ee36e3bd8
nakae-t <2301180@s.asojuku.ac.jp>
2024-11-18 10:56:19 +0900

    11/18

--- Reidai/script/script.js
+++ Reidai/script/script.js
@@ -3,14 +3,15 @@ new Vue({
     el: '#app',
     
     data: {
+      pass:'',
+      passcheck:''
     },
     computed: {
-      // 入力されている、且つパスワードが一致しているかを確認する算出プロパティ
-      // 入力されているかは、this.･･･ で判定可能
-      // 例）一致TRUE（パスワード：aaaa、パスワード確認：aaaa）
-      // 例）不一致FALSE（パスワード：aaaa、パスワード確認：aaab）
-
-      // ボタンをTRUEなら登録を表示orFALSEなら使用不可を表示にする算出プロパティ
-
-    }
+      passMismatch(){
+        return this.pass && this.passcheck && this.pass !== this.passcheck;
+        },
+      canRegister(){
+        return this.pass && this.passcheck && !this.passMismatch;
+      }
+      }
   });
